8a203a0acd73833ce00bc0cce6bf8f2951b880f4
Tatarinov_Vladislav <v.maxn@mail.ru>
2025-01-11 19:04:06 +0500

    2 zadanie

---
+++ MathUtils.py
@@ -0,0 +1,20 @@
+class MathUtils:
+    def __init__(self, value_1, value_2):
+        self.value_1 = value_1
+        self.value_2 = value_2
+
+
+    def summation(self):
+        return self.value_1 + self.value_2
+
+
+    def difference(self):
+        return self.value_1 - self.value_2
+
+
+    def multiplication(self):
+        return self.value_1 * self.value_2
+
+
+    def division(self):
+        return self.value_1 / self.value_2
