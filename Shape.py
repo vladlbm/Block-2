01154d92661323caf546a6834e11ac50a4e076f5
Tatarinov_Vladislav <v.maxn@mail.ru>
2025-01-14 11:20:19 +0500

    10 zadanie

---
+++ Shape.py
@@ -0,0 +1,39 @@
+import math
+
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+
+    def area(self):
+        """Переопределяющий метод для вычисления площади в других подклассах"""
+        raise NotImplementedError(f'Метод area требует определения в классе: {self.__class__.__name__}')
+
+
+    def name_info(self):
+        print(f'Фигура: {self.name}')
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__('Rectangle')
+        self.width = width
+        self.height = height
+
+
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__('Circle')
+        self.radius = radius
+
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+
+
